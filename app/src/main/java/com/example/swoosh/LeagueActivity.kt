6a032b1544f9dee83e80244cdea44459bf46ed58
Shubham Kashyap <shubham.kashyap.jha07@gmail.com>
2024-04-30 18:38:47 +0530

    lesson 4

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,16 +1,23 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_league)
 
+    }
+
+    fun leagueNextClicked(view: View){
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : BaseActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
